b7da5b80612009584d4e9b78341440198db11fde
jakaria26 <jamahmud2022@gmail.com>
2026-04-13 00:29:26 -0400

    Update font.js

--- font.js
+++ font.js
@@ -1,1 +1,5 @@
-
+const fontButton = document.querySelector("button[id='font-button']");
+fontButton.addEventListener("click", () => {
+const fontSelect = document.querySelector("select[id='font-select']").value;
+document.body.style.fontFamily = fontSelect;
+});
