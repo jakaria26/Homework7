a85fb1f0eb06e298ea95667a2c278cf57a933483
jakaria26 <jamahmud2022@gmail.com>
2026-04-13 00:03:07 -0400

    Create color.js

---
+++ color.js
@@ -0,0 +1,5 @@
+const colorButton = document.querySelector("button[id='color-button']");
+colorButton.addEventListener("click", () => {
+let colorInput = document.querySelector("input[id='color-input']").value;
+document.body.style.backgroundColor = colorInput;
+});
