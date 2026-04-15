5d43b6b8a8dd362e308da047a019466bd748f4e4
jakaria26 <jamahmud2022@gmail.com>
2026-04-15 15:20:31 -0400

    Update other.js

--- other.js
+++ other.js
@@ -1,1 +1,48 @@
+const elRadio = document.querySelector("input[id='js-topic-eventlisteners']");
+const jqRadio = document.querySelector("input[id='js-topic-jquery']");
+const fRadio = document.querySelector("input[id='js-topic-fetch']");
+const otherRadio = document.querySelector("input[id='js-topic-other']");
+let clickstatus = 0;
 
+otherRadio.addEventListener("click", () => {
+    checkRadio("other");
+});
+elRadio.addEventListener("click", () => {
+checkRadio("existing");
+});
+jqRadio.addEventListener("click", () => {
+checkRadio("existing");
+});
+fRadio.addEventListener("click", () => {
+checkRadio("existing");
+});
+
+async function checkRadio(currentRadio){
+const sendRadio = await fetch(other.php?radio=${currentRadio});
+let result = await sendRadio.json();
+if (result["addDelete"] == "add"){
+addTextBox();
+    } else {
+deleteTextBox();
+    }
+}
+
+function addTextBox(){
+if (clickstatus == 0){
+const tbInput = document.createElement("input");
+tbInput.setAttribute("name", "js-topic");
+tbInput.setAttribute("type", "text");
+tbInput.setAttribute("id", "js-topic-other-tb");
+const currentElement = document.querySelector("label[id='other-label']");
+currentElement.after(tbInput);
+clickstatus++;
+    }
+}
+
+function deleteTextBox(){
+if (clickstatus > 0){
+clickstatus = 0;
+const tbInput = document.querySelector("#js-topic-other-tb");
+        tbInput.remove();
+    }
+}
