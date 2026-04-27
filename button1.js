296c86a3645378f3ede1e9c0d079c5bbe7e45043
jakaria26 <jamahmud2022@gmail.com>
2026-04-26 21:12:07 -0400

    Update button1.js

--- button1.js
+++ button1.js
@@ -1,4 +1,4 @@
-let countButton = document.querySelector("button[id='counting-button']");
+const countButton = document.querySelector("button[id='counting-button']");
 let count = 0;
 
 countButton.addEventListener("click", (event) => {
